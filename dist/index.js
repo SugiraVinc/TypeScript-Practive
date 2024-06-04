85abf114cf8914b316d2eb3fd3fd82c418e937e5
SugiraVinc <sugiravincentg11@gmail.com>
2024-06-04 09:48:28 +0300

    typescript basics

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Basic Types
+let id = 5;
+console.log('ID', id);
+let comapanyName = 'Poetherapy Ink LTD';
+let isPublished = true;
+let x = 'Welcome';
+let ids = [22, 32, 44, 5, 46, 34];
+let arr = [1, true, , 'Welcome'];
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// Basic Types
+let id: number = 5
+console.log('ID', id)
+
+let comapanyName: String = 'Poetherapy Ink LTD'
+let isPublished: boolean = true
+let x: any = 'Welcome'
+
+let ids: number[] = [22,32,44,5,46,34]
+let arr: any[] =  [1,true,, 'Welcome']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Sugira', true]
+
+// Tuple array
+
+let employee: [number, string][]
+
+employee =[
+    [1, 'Vinc'],
+    [2, 'Sugira'],
+    [3, 'Prince']
+]
+
+//
