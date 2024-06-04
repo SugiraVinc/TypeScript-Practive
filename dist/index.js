db7ae6d1ce5b647ea49221f86696468e66372a53
SugiraVinc <sugiravincentg11@gmail.com>
2024-06-04 09:50:13 +0300

    file configuration

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,13 @@ let isPublished = true;
 let x = 'Welcome';
 let ids = [22, 32, 44, 5, 46, 34];
 let arr = [1, true, , 'Welcome'];
+// Tuple
+let person = [1, 'Sugira', true];
+// Tuple array
+let employee;
+employee = [
+    [1, 'Vinc'],
+    [2, 'Sugira'],
+    [3, 'Prince']
+];
+//
